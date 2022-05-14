ef49c1e687abcc8fb1d3146938235ac0504778cb
Alexis Xiu <expensive_12345@163.com>
2022-05-14 15:43:25 +0800

    First Demo version.

---
+++ source/launch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	cout<<"=========Welcome to MovingXenia Legend========="<<endl;
+	cout<<"Press any key to start the gane."<<endl;
+	getch();
+	system("@start SetMap");
+	system("@Moving");
+	return 0;
+}
