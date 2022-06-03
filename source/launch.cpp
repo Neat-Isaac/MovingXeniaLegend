e7ab076f5797b806bb05ff0baf6087fab1ebf20a
Alexis Xiu <expensive_12345@163.com>
2022-06-03 20:55:36 +0800

    The level and things

---
+++ source/launch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	cout<<"=========Welcome to MovingXenia Legend========="<<endl;
+	cout<<"Press any key to start the game."<<endl;
+	getch();
+	system("@Moving");
+	return 0;
+}
